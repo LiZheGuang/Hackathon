8db334719f4ff6d215efd26619ba529aaa6507f9
LiZheGuang <1030660726@qq.com>
2018-11-16 20:42:49 +0800

    no message

--- pages/answer/answer.js
+++ pages/answer/answer.js
@@ -5,14 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    buttonImage:""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let assignment = wx.getStorageSync('assignment')
+    console.log(assignment)
+    this.setData({
+      buttonImage: assignment.data
+    })
   },
 
   /**
